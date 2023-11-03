c30b2cb952b84927e2c1e21ef24d1dbc2f831321
Jackson Nace <jacksonnace964@gmail.com>
2023-11-02 20:46:53 -0500

    convertversion

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-import OpenAI from 'openai';
-import {} from 'dotenv';
+import { config } from "dotenv"
+config()
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-});
+import { OpenAI } from "openai";
+import readline from "readline"
 
-const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{"role": "user", "content": "Hello!"}],
-  });
-  console.log(chatCompletion.choices[0].message);
+const openai = new OpenAI({ apiKey: process.env.TOKEN });
+
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+userInterface.prompt()
+userInterface.on("line", async input=> {
+  const res = await openai.chat.completions.create({
+    model:"gpt-3.5-turbo",
+    messages: [{ role: "user", content: input }],
+  })
+  console.log(res.data.choices[0].message.content)
+  userInterface.prompt()
+})
 
 
